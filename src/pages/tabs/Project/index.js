fd5e02ec1ca77c6730cd76980341d5e536299325
王聪 <goastwang0131@gmail.com>
2021-05-06 22:18:09 +0800

    项目中心静态页面（下拉刷新\上拉加载功能待实现）

--- src/pages/tabs/Project/index.js
+++ src/pages/tabs/Project/index.js
@@ -1,16 +1,85 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, memo } from 'react';
+import { View, FlatList, TouchableHighlight, Text, ActivityIndicator } from 'react-native';
 import styles from './styles';
+import { SearchBar } from 'react-native-elements';
+import common from '../../../styles/common';
+import { colors } from '../../../styles/variable';
 
-const Project = () => {
-  const handleLogin = e => {
-    console.log('嘻嘻嘻爱', e);
+const Project = memo(() => {
+  const [keyword, setKeyword] = useState('');
+  const handleSearch = value => {
+    setKeyword(value);
   };
   return (
     <View style={styles.container}>
-      <Text>项目中心</Text>
+      <SearchBar
+        containerStyle={styles.searchBarContainer}
+        inputStyle={styles.searchInput}
+        inputContainerStyle={styles.searchInputContainer}
+        lightTheme={true}
+        placeholder="搜索"
+        onChangeText={handleSearch}
+        value={keyword}
+      />
+      <FlatList
+        style={styles.flatListContainer}
+        data={[
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' },
+          { title: 'Title Text', key: 'item1' }
+        ]}
+        numColumns="2"
+        renderItem={({ item, index, separators }) => (
+          <TouchableHighlight
+            // onPress={() => this._onPress(item)}
+            onShowUnderlay={separators.highlight}
+            onHideUnderlay={separators.unhighlight}
+          >
+            <View style={styles.listItem}>
+              <Text
+                style={[
+                  common.iconStyle,
+                  { color: colors.themeColor, fontSize: 80 }
+                ]}
+              >
+                {'\ue74f'}
+              </Text>
+              <Text style={styles.projectName}>碧桂园一期</Text>
+              <Text style={styles.createTimeText}>08-08 15:36</Text>
+            </View>
+          </TouchableHighlight>
+        )}
+        ItemSeparatorComponent={
+          // eslint-disable-next-line no-undef
+          Platform.OS !== 'android' &&
+          (({ highlighted }) => (
+            <View
+              style={[styles.separator, highlighted && { marginLeft: 0 }]}
+            />
+          ))
+        }
+      />
     </View>
   );
-};
+});
 
 export default Project;
--- src/pages/tabs/Project/styles.js
+++ src/pages/tabs/Project/styles.js
@@ -1,17 +1,51 @@
 import { StyleSheet } from 'react-native';
 import { screenSize } from '../../../utils/tools';
+import { colors } from '../../../styles/variable';
 
 const styles = StyleSheet.create({
   container: {
     width: screenSize.width,
-    height: screenSize.height,
-    alignItems: 'center'
+    height: '100%'
   },
-  iconStyle: {
-    fontFamily: 'iconfont',
-    fontSize: 24,
-    marginTop: 10,
-    marginLeft: 10
+  separator: {
+    height: 1
+  },
+  searchBarContainer: {
+    width: '100%',
+    backgroundColor: colors.bgGray,
+    borderTopWidth: 0,
+    borderBottomWidth: 0
+  },
+  searchInputContainer: {
+    backgroundColor: '#fff'
+  },
+  searchInput: {
+    backgroundColor: '#fff'
+  },
+  flatListContainer: {
+    width: '100%',
+    flex: 1
+  },
+  listItem: {
+    width: screenSize.width / 2,
+    height: 220,
+    backgroundColor: '#fff',
+    borderRightColor: colors.bgGray,
+    borderRightWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  projectName: {
+    fontSize: 16,
+    color: '#262626',
+    lineHeight: 24,
+    marginTop: 30,
+    marginBottom: 2
+  },
+  createTimeText: {
+    fontSize: 12,
+    color: '#8c8c8c',
   }
 });
 
